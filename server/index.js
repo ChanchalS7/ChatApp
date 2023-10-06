96f8b421097c6046f190f2494e0a05ddba104a04
ritu1011 <chsverma7@gmail.com>
2023-10-07 00:06:28 +0530

    add chat app using mern stack

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const cors = require('cors');
+const { Server } = require('socket.io')
+app.use(cors());
+const server = http.createServer(app);
+
+const io = new Server(server, {
+	cors: {
+		origin: "http://localhost:5173",
+		methods: ['GET', 'POST'],
+	}
+})
+io.on('connection', (socket) => {
+	console.log(`User connected:${socket.id}`)
+	socket.on('send-message', (message) => {
+		console.log("Message:", message)
+		//Broadcast the received message to all the connection
+		io.emit('received-message', message)
+	})
+	socket.on('disconnect', () => console.log(`User disconnected`))
+})
+
+server.listen(5000, () => {
+	console.log(`Server running at port 5000`)
+})
